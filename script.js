da0df78e8d18316b216b404c698686677b85d509
Lukrex <89094596+Lukrex@users.noreply.github.com>
2022-08-19 14:02:14 +0200

    Lessons work

--- script.js
+++ script.js
@@ -5,27 +5,28 @@ let exit = document.getElementById("exit"); //iframe
 //for every element with class name "unit"
 for (let unit of units) {
     unit.addEventListener('click', function() {
-        console.log("clicked unit!");
+        console.log("unit");
         if (unit.firstElementChild.style.display == "none") {
             unit.firstElementChild.classList.add("active");
             unit.firstElementChild.classList.remove("close");
             unit.firstElementChild.style.display = "initial";
             setTimeout(()=>{ unit.lastElementChild.style.display="block"; }, "500");
-        } else { console.log("uh oh"); }
+        }
     })
 }
 
-courseSelector.addEventListener("change", function() {
-//change link!! //courseSelector value must be in link for course
-    let pickedCourse = courseSelector.value;
-    window.location.href = pickedCourse + ".html";
-})
-
 function exitIFrame(exitBtn) {
-    console.log("exit");
     exitBtn.style.display = "none";
-    let iframe = exitBtn.parentElement.firstElementChild;
-    iframe.classList.remove("active");
-    iframe.classList.add("close");
-    setTimeout(()=>{ iframe.style.display="none"; }, "500");
+    let lframe = exitBtn.parentElement.firstElementChild;
+    lframe.classList.remove("active");
+    lframe.classList.add("close");
+    setTimeout(()=>{ lframe.style.display="none"; }, "500");
+}
+
+if (courseSelector) {
+    courseSelector.addEventListener("change", function() {
+    //change link!! //courseSelector value must be in link for course
+        let pickedCourse = courseSelector.value;
+        window.location.href = pickedCourse + ".html";
+    })
 }
